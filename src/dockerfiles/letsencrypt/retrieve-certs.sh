38e4311753f2af63f3577d83e26252d2eea0e7d6
Michael Bond <software@the-forgotten.org>
2020-12-30 07:41:44 -0500

    WIP: Let's Encrypt -- Handle sub domains

--- src/dockerfiles/letsencrypt/retrieve-certs.sh
+++ src/dockerfiles/letsencrypt/retrieve-certs.sh
@@ -8,7 +8,8 @@ cd $LETS_ENCRYPT_DIR
 docker build -t lets-encrypt-apache .
 mkdir -p $LETS_ENCRYPT_DIR/etc
 
-DOMAINS=("morgantown.ninja" "the-forgotten.org" "grapevine.the-forgotten.org" "baiyingpai.com" "emmasbond.me" "fairmontflyers.org" "kathnmike.us" "whiteeaglemartialarts.com")
+DOMAINS=("morgantown.ninja" "the-forgotten.org" "baiyingpai.com" "emmasbond.me" "fairmontflyers.org" "kathnmike.us" "whiteeaglemartialarts.com")
+SUB_DOMAINS=("grapevine.the-forgotten.org")
 
 for DOMAIN in "${DOMAINS[@]}"; do
   export SERVER_NAME=$DOMAIN
@@ -27,6 +28,28 @@ for DOMAIN in "${DOMAINS[@]}"; do
   --webroot-path=/data/letsencrypt \
   --force-renewal \
   -d $SERVER_NAME -d www.${SERVER_NAME}
+
+  docker-compose down
+  sleep 10
+done
+
+for DOMAIN in "${SUB_DOMAINS[@]}"; do
+  export SERVER_NAME=$DOMAIN
+
+  docker-compose up -d
+  sleep 10
+
+  sudo docker run -it --rm \
+  -v $LETS_ENCRYPT_DIR/etc:/etc/letsencrypt \
+  -v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
+  -v $PWD/html:/data/letsencrypt \
+  -v /docker-volumes/var/log/letsencrypt:/var/log/letsencrypt \
+  certbot/certbot \
+  certonly --webroot \
+  --email mbond@morgantown.ninja --agree-tos --no-eff-email \
+  --webroot-path=/data/letsencrypt \
+  --force-renewal \
+  -d $SERVER_NAME
 
   docker-compose down
   sleep 10
