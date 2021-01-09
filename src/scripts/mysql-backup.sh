8ae9cc69f099602555ade113edad2ff2172544f8
Michael Bond <software@the-forgotten.org>
2021-01-09 09:35:44 -0500

    MySQL Backup

---
+++ src/scripts/mysql-backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+MYSQL_BACKUP_DIR=/mnt/tfo-volume-01/backups/mysql
+mkdir -p "${MYSQL_BACKUP_DIR}"
+
+MONTH=$(date +%m)
+DAY=$(date +%d)
+YEAR=$(date +%Y)
+
+BACKUP_FILENAME="grapevine_rss.backup.${YEAR}.${MONTH}.${DAY}.sql"
+docker exec src_grapevine-mysql_1 /usr/bin/mysqldump -u grapevine --password="${DB_PASSWD}" grapevine_rss > "${MYSQL_BACKUP_DIR}/${BACKUP_FILENAME}"
