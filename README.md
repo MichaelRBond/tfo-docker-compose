9424b977c8ac17c37355c3c62a3703ab430f6511
Michael Bond <software@the-forgotten.org>
2021-01-09 19:57:20 -0500

    volume snapshot management script

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 docker build -f httpd.dockerfile -t mrbond/tfo-httpd:0.x .
 docker login -u xxxx -p xxxx
 docker push mrbond/tfo-httpd:0.x
+
+# Enabling at startup
+
+1. sudo systemctl enable docker
+1. docker-compose up -d
---
+++ src/scripts/manage_volume_snapshots.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+TWO_WEEKS_IN_SEC=1209600
+NOW=$(date +%s)
+
+# Setup
+
+# Check that jq is installed
+if ! [ -x "$(command -v jq)" ]; then
+  yum install -y jq
+fi
+
+# Take a snapshot of the current volume
+SNAPSHOT_NAME="tfo-volume-01-${NOW}"
+echo "Creating a snapshot: ${SNAPSHOT_NAME}"
+curl -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $DO_API_TOKEN" -d '{"name":"'"${SNAPSHOT_NAME}"'"}' "https://api.digitalocean.com/v2/volumes/$TFO_VOLUME_01_ID/snapshots"
+
+# Delete any snapshots that are older than 2 weeks
+mapfile -t  ALL_SNAPSHOTS < <(curl -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $DO_API_TOKEN" "https://api.digitalocean.com/v2/snapshots" | jq -c '.snapshots |= sort_by(.created_at) | .snapshots[] | select(.resource_type=="volume")')
+
+for snapshot in "${ALL_SNAPSHOTS[@]}"; do
+  CREATED_AT=$(echo $snapshot | jq -r .created_at)
+  DATE_AS_EPOCH=$(date -d ${CREATED_AT} +"%s")
+  ID=$(echo $snapshot | jq -r .id)
+
+  DIFF_IN_DATES=$(expr $NOW - $DATE_AS_EPOCH)
+  if [[ $DIFF_IN_DATES -gt $TWO_WEEKS_IN_SEC ]]; then
+    echo "Deleting ID=${ID} Created at=${CREATED_AT}"
+    curl -X DELETE -H 'Content-Type: application/json' -H "Authorization: Bearer $DO_API_TOKEN" "https://api.digitalocean.com/v2/snapshots/${ID}"
+  fi
+done
